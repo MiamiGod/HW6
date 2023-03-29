709deb7a653dd5580a4d258798adce5b1b1f86ab
Ilya.Veretennikov <evolutionlena672@gmail.com>
2023-03-30 00:39:48 +0700

    addhw7

--- Program.cs
+++ Program.cs
@@ -1,42 +1,155 @@
-﻿// // Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел строго больше 0 ввёл пользователь.
-// // Method
-// // int CountPositiveNums(string[] arr)
-// // {
-// //     int count = 0;
-// //     for (int i = 0; i < arr.Length; i++)
-// //     {
-// //         int value = int.Parse(arr[i]);
-// //         if (value > 0) count++;
-// //     }
-// //     return count;
-// // }
+﻿// // // Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами
+// // //Methods
 
-// // Code
-// // Console.WriteLine("Введите несколько целых чисел через пробел");
-// // string[] array = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
-// // Console.WriteLine($"Количество введенных положительных чисел: {CountPositiveNums(array)}");
-
-// //  Написать программу, которая на вход принимает массив из любого количества элементов (не менее 6)в промежутке от 0 до 100, 
-// // а на выходе выводит этот же массив, но отсортированный по возрастанию(от меньшего числа к большему).
-
-
-
-// // Method
-// int[] GetArray(int size)
+// int[,] GetArray(int m, int n, int min, int max)
 // {
-//     int[] array = new int[size];
-//     for (int i = 0; i < size; i++)
+//     int[,] result = new int[m, n];
+//     for (int i = 0; i < m; i++)
 //     {
-//         array[i] = new Random().Next(0, 101);
+//         for (int j = 0; j < n; j++)
+//         {
+//             result[i, j] = new Random().Next(min, max);
+//         }
 //     }
-//     return array;
+//     return result;
 // }
 
-// // Code
-// Console.Write("Введите размер массива: ");
-// int a = int.Parse(Console.ReadLine()!);
-// var num = GetArray(a);
-// Console.WriteLine($"[{String.Join(", ", num)}]");
-// Array.Sort(num);
-// foreach (var i in num)
-//     Console.Write($"{i} ");
+// void PrintArray(int[,] localArray)
+// {
+//     for (int i = 0; i < localArray.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < localArray.GetLength(1); j++)
+//         {
+//             Console.Write($"{localArray[i, j]} \t");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// // //Code
+
+// Console.WriteLine("Введите количество строк:");
+// int rows = int.Parse(Console.ReadLine());
+// Console.WriteLine("Введите количество столбцов:");
+// int columns = int.Parse(Console.ReadLine());
+// int[,] myArray = GetArray(rows, columns, 0, 101);
+// Console.WriteLine("Ваш массив:");
+// PrintArray(myArray);
+
+
+// //  Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+// // и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+// int[,] GetArray(int m, int n, int min, int max)
+// {
+//     int[,] result = new int[m, n];
+//     for (int i = 0; i < m; i++)
+//     {
+//         for (int j = 0; j < n; j++)
+//         {
+//             result[i, j] = new Random().Next(min, max);
+//         }
+//     }
+//     return result;
+// }
+
+// void PrintArray(int[,] localArray)
+// {
+//     for (int i = 0; i < localArray.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < localArray.GetLength(1); j++)
+//         {
+//             Console.Write($"{localArray[i, j]} \t");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// void FindElement(int[,] inArray, int x, int y)
+// {
+//     if (x >= inArray.GetLength(0) || y >= inArray.GetLength(1) || x < 1 || y < 1)
+//     {
+//         Console.WriteLine("Такого элемента в массиве нет");
+//     }
+//     else Console.WriteLine($"Значение выбранного элемента: {inArray[x, y]}");
+// }
+
+
+// // //Code
+
+// Console.WriteLine("Введите количество строк:");
+// int rows = int.Parse(Console.ReadLine());
+// Console.WriteLine("Введите количество столбцов:");
+// int columns = int.Parse(Console.ReadLine());
+
+// int[,] myArray = GetArray(rows, columns, 0, 101);
+// Console.WriteLine("Ваш массив:");
+// PrintArray(myArray);
+
+// Console.WriteLine("Введите номер строки выбранного элемента:");
+// int rowNumber = int.Parse(Console.ReadLine()) - 1;
+// Console.WriteLine("Введите номер столбца выбранного элемента:");
+// int columnNumber = int.Parse(Console.ReadLine()) - 1;
+// FindElement(myArray, rowNumber, columnNumber);
+
+
+// // задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+// // Method
+
+
+// int[,] GetArray(int m, int n, int min, int max)
+// {
+//     int[,] result = new int[m, n];
+//     for (int i = 0; i < m; i++)
+//     {
+//         for (int j = 0; j < n; j++)
+//         {
+//             result[i, j] = new Random().Next(min, max);
+//         }
+//     }
+//     return result;
+// }
+
+// void PrintArray(int[,] localArray)
+// {
+//     for (int i = 0; i < localArray.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < localArray.GetLength(1); j++)
+//         {
+//             Console.Write($"{localArray[i, j]} \t");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// int[] FindColumnAverage(int[,] arr)
+// {
+
+//     int size = arr.GetLength(1);
+//     int[] average = new int[size];
+
+//     for (int j = 0; j < arr.GetLength(1); j++)
+//     {
+//         int sum = 0;
+//         for (int i = 0; i < arr.GetLength(0); i++)
+//         {
+//             sum = sum + arr[i, j];
+//         }
+//         int columnAverage = sum / arr.GetLength(0);
+//         average[j] = columnAverage;
+//     }
+//     return average;
+// }
+
+// //Code
+
+// Console.WriteLine("Введите количество строк:");
+// int rows = int.Parse(Console.ReadLine());
+// Console.WriteLine("Введите количество столбцов:");
+// int columns = int.Parse(Console.ReadLine());
+// int[,] myArray = GetArray(rows, columns, 0, 11);
+// Console.WriteLine("Ваш массив:");
+// PrintArray(myArray);
+
+// Console.WriteLine("Cреднее арифметическое элементов в каждом столбце: \n{0}", String.Join("\t", FindColumnAverage(myArray)));
